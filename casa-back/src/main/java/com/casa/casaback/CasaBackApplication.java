8cdf7557402fd6f4a121c7d0515ce2a1d6ecc49f
nemanja3214 <nemanja.majstorovic3214@gmail.com>
2023-11-15 14:32:47 +0100

    Init

---
+++ casa-back/src/main/java/com/casa/casaback/CasaBackApplication.java
@@ -0,0 +1,13 @@
+package com.casa.casaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaBackApplication.class, args);
+	}
+
+}
