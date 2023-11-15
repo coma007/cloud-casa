700e6cc766f3d4e68cb83ab62c500fc6c1f029e1
nemanja3214 <nemanja.majstorovic3214@gmail.com>
2023-11-15 22:20:21 +0100

    Added locations

---
+++ casa-back/src/main/java/com/casa/app/location/City.java
@@ -0,0 +1,22 @@
+package com.casa.app.location;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="country_id", nullable=false)
+    private Country country;
+}
---
+++ casa-back/src/main/java/com/casa/app/location/CityRepository.java
@@ -0,0 +1,4 @@
+package com.casa.app.location;
+
+public class CityRepository {
+}
