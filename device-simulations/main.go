872547887709b76178c7ab562a51dc4889c7d3bc
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2023-11-19 22:32:43 +0100

    feat:mqtt setup

--- device-simulations/main.go
+++ device-simulations/main.go
@@ -1,1 +1,51 @@
-package device_simulations
+package main
+
+import (
+	"fmt"
+	"github.com/eclipse/paho.mqtt.golang"
+	"os"
+	"time"
+)
+
+func messageHandler(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+}
+
+func main() {
+	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883") // MQTT broker URL
+	opts.SetClientID("golang-app")                                    // Client ID
+	opts.SetUsername("admin")
+	opts.SetPassword("12345678")
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+	defer client.Disconnect(250)
+
+	// Publish messages or subscribe to topics as needed
+	topic := "myTopic" // Specify the topic to which you want to publish
+	message := "Hello, MQTT from Golang"
+
+	token := client.Publish(topic, 0, false, message)
+	token.Wait()
+
+	fmt.Printf("Published message: %s to topic: %s\n", message, topic)
+	time.Sleep(2 * time.Second)
+
+	topic = "serverMessages" // Specify the topic you want to subscribe to
+
+	if token := client.Subscribe(topic, 0, messageHandler); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("Subscribed to topic: %s\n", topic)
+
+	// Keep the program running to receive messages
+	// For demonstration purposes, you can add a sleep or use a blocking mechanism
+	// You might use a channel, select statement, or other mechanisms in an actual application
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
