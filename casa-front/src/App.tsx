9572eb050cbbd3b5e5ef9830060b5ed5450ef045
nemanja3214 <nemanja.majstorovic3214@gmail.com>
2023-12-10 13:15:16 +0100

    Removed multiple fav icon requests

--- casa-front/src/App.tsx
+++ casa-front/src/App.tsx
@@ -1,11 +1,20 @@
 import Router from './routes/Router';
 import style from './App.module.scss'
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <div className={style.App}>
-      <Router></Router>
-    </div>
+    <>
+      <Helmet>
+        <title>My App</title>
+        <link rel="canonical" href="http://example.com" />
+        <link rel="icon" href="data:image/x-icon;," type="image/x-icon" /> 
+        <meta name="description" content="My app description" />
+      </Helmet>
+      <div className={style.App}>
+        <Router></Router>
+      </div>
+    </>
   );
 }
 
