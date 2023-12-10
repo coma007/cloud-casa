a772aa0f188355179d04af3cbfcd5dcf65112458
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2023-12-10 02:38:48 +0100

    feat: Added image button

---
+++ casa-front/src/components/tables/ImageButton/ImageButton.tsx
@@ -0,0 +1,15 @@
+import Tooltip from "../../view/Tooltip/Tooltip";
+import ImageButtonCSS from "./ImageButton.module.scss"
+import TooltipCSS from "components/view/Tooltip/Tooltip.module.scss"
+import { MouseEventHandler } from "react";
+
+const ImageButton = (props: { path: string, tooltipText: string, onClick: MouseEventHandler<HTMLButtonElement> }) => {
+    return (
+        <button onClick={props.onClick} className={`${ImageButtonCSS.button} ${TooltipCSS.bottomTooltip}`}>
+            <img src={props.path}></img>
+            <Tooltip tooltipText={props.tooltipText} />
+        </button>
+    )
+}
+
+export default ImageButton;
