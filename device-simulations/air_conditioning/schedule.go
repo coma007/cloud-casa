22cfc7d06d6d04bc671221289cdd2d3c345bfcfe
nemanja3214 <nemanja.majstorovic3214@gmail.com>
2024-01-30 14:35:32 +0100

    Moved scheduling logic to simulator

---
+++ device-simulations/air_conditioning/schedule.go
@@ -0,0 +1,42 @@
+package air_conditioning
+
+import "time"
+
+type AirConditioningSchedule struct {
+	id int64
+
+	StartTime CustomTime `json:"startTime"`
+	EndTime   CustomTime `json:"endTime"`
+
+	Override  bool `json:"override"`
+	Activated bool `json:"activated"`
+
+	Working     bool     `json:"working"`
+	Mode        *string  `json:"mode"`
+	Temperature *float64 `json:"temperature"`
+
+	Repeating              bool  `json:"repeating"`
+	RepeatingDaysIncrement int64 `json:"repeatingDaysIncrement"`
+}
+
+type CustomTime struct {
+	time.Time
+}
+
+func (t *CustomTime) UnmarshalJSON(b []byte) (err error) {
+	date, err := time.Parse(`"2006-01-02T15:04:05"`, string(b))
+	if err != nil {
+		return err
+	}
+	t.Time = date
+	//loc, err := time.LoadLocation("Europe/Belgrade")
+	//t.Time = t.Time.In(loc)
+	return
+}
+
+func TimeIsBetween(t, min, max time.Time) bool {
+	if min.After(max) {
+		min, max = max, min
+	}
+	return (t.Equal(min) || t.After(min)) && (t.Equal(max) || t.Before(max))
+}
