260ccdd9011c18a9462296d951952863fafc0bf7
coma007 <milicasladakovic@gmail.com>
2024-01-30 18:39:12 +0100

    feat: scheduling

--- casa-back/src/main/java/com/casa/app/device/outdoor/sprinkler_system/SprinklerSystemController.java
+++ casa-back/src/main/java/com/casa/app/device/outdoor/sprinkler_system/SprinklerSystemController.java
@@ -9,10 +9,7 @@ import jakarta.annotation.security.PermitAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,7 +29,14 @@ public class SprinklerSystemController {
     @PermitAll
     @GetMapping("/turnOn/{id}")
     public ResponseEntity<?> toggleOn(@PathVariable Long id) throws NotFoundException, UserNotFoundException {
-//        sprinklerSystemService.toggleOn(id);
+        sprinklerSystemService.toggleOn(id);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PermitAll
+    @GetMapping("/setSchedule/{id}")
+    public ResponseEntity<?> toggleOn(@PathVariable Long id, @RequestBody SprinklerSystemSchedule schedule) throws NotFoundException, UserNotFoundException {
+        sprinklerSystemService.setSchedule(id, schedule);
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
--- casa-back/src/main/java/com/casa/app/device/outdoor/sprinkler_system/SprinklerSystemService.java
+++ casa-back/src/main/java/com/casa/app/device/outdoor/sprinkler_system/SprinklerSystemService.java
@@ -5,15 +5,23 @@ import com.casa.app.device.outdoor.lamp.Lamp;
 import com.casa.app.device.outdoor.lamp.LampRepository;
 import com.casa.app.device.outdoor.lamp.dto.LampSimulationDTO;
 import com.casa.app.device.outdoor.sprinkler_system.dto.SprinklerSystemSimulationDTO;
+import com.casa.app.exceptions.NotFoundException;
+import com.casa.app.exceptions.UserNotFoundException;
 import com.casa.app.influxdb.InfluxDBService;
 import com.casa.app.mqtt.MqttGateway;
+import com.casa.app.user.regular_user.RegularUser;
 import com.casa.app.user.regular_user.RegularUserService;
 import com.casa.app.websocket.WebSocketController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SprinklerSystemService {
@@ -29,6 +37,27 @@ public class SprinklerSystemService {
     @Autowired
     private RegularUserService regularUserService;
 
+    public void toggleOn(Long id) throws NotFoundException, UserNotFoundException {
+        RegularUser currentUser = regularUserService.getUserByToken();
+        Optional<SprinklerSystem> sprinklerSystem = sprinklerSystemRepository.findById(id);
+        if (sprinklerSystem.isEmpty()) {
+            throw new NotFoundException();
+        }
+        mqttGateway.sendToMqtt(id + "~ON~" + currentUser.getUsername(), id.toString());
+    }
+
+    public void setSchedule(Long id, SprinklerSystemSchedule schedule) throws NotFoundException, UserNotFoundException {
+        RegularUser currentUser = regularUserService.getUserByToken();
+        Optional<SprinklerSystem> sprinklerSystem = sprinklerSystemRepository.findById(id);
+        if (sprinklerSystem.isEmpty()) {
+            throw new NotFoundException();
+        }
+        String scheduleJson = getSprinklerJson(schedule);
+        sprinklerSystem.get().setSchedule(schedule);
+        sprinklerSystemRepository.save(sprinklerSystem.get());
+        mqttGateway.sendToMqtt(id + "~SCHEDULE~" + currentUser.getUsername() + "|" + scheduleJson, id.toString());
+    }
+
     public List<SprinklerSystemSimulationDTO> getAllSimulation() {
         List<SprinklerSystem> sprinklers = sprinklerSystemRepository.findAll();
         List<SprinklerSystemSimulationDTO> sprinklersDTOS = new ArrayList<>();
@@ -37,5 +66,19 @@ public class SprinklerSystemService {
         }
         return sprinklersDTOS;
     }
+
+    private static String getSprinklerJson(SprinklerSystemSchedule schedule) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        String scheduleJson;
+        try {
+            scheduleJson = objectMapper.writeValueAsString(schedule);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Error converting SprinklerSystemSchedule to JSON", e);
+        }
+        return scheduleJson;
+    }
+
 
 }
