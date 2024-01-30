039e88cdbed6d4ea825f8285dc898267aec7ef1e
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2024-01-30 19:34:30 +0100

    feat: Power usage controller

--- casa-back/src/main/java/com/casa/app/device/large_electric/house_battery/HouseBatteryController.java
+++ casa-back/src/main/java/com/casa/app/device/large_electric/house_battery/HouseBatteryController.java
@@ -1,12 +1,16 @@
 package com.casa.app.device.large_electric.house_battery;
 
+import com.casa.app.device.large_electric.house_battery.dto.CityPowerUsageDTO;
 import com.casa.app.device.large_electric.house_battery.dto.HouseBatterySimulationDTO;
+import com.casa.app.device.large_electric.house_battery.dto.RealEstatePowerUsageDTO;
 import jakarta.annotation.security.PermitAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,5 +28,20 @@ public class HouseBatteryController {
         return new ResponseEntity<>(houseBatteryService.getAllSimulation(), HttpStatus.OK);
     }
 
-    
+    @PreAuthorize("hasAnyAuthority('admin')")
+    @GetMapping("/powerUsage")
+    public ResponseEntity<List<RealEstatePowerUsageDTO>> getPowerUsage(@RequestParam String from,
+                                           @RequestParam String to) {
+        List<RealEstatePowerUsageDTO> estates = houseBatteryService.powerUsageByEstate(from, to);
+        return new ResponseEntity<>(estates, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAnyAuthority('admin')")
+    @GetMapping("/powerUsageForCity")
+    public ResponseEntity<CityPowerUsageDTO> getPowerUsage(@RequestParam String from,
+                                           @RequestParam String to,
+                                           @RequestParam String cityName) {
+        CityPowerUsageDTO city = houseBatteryService.powerUsageByCity(from, to, cityName);
+        return new ResponseEntity<>(city, HttpStatus.OK);
+    }
 }
